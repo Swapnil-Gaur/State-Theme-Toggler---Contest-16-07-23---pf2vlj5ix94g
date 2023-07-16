5c635f1595102442ce8abfb0c62339ee02cb1159
Swapnil <swapnilchaudhari0321@gmail.com>
2023-07-16 11:42:07 +0530

    new commit

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,11 @@ import React, { useState } from "react";
 import "../styles/App.css";
 
 export default function App() {
+    const[theme , setTheme]=useState(light);
+    const toggleTheme=()=>{
+        setTheme(pretheme=>(pretheme==='light'?'dark':'light'));
+    }
+    
     return (
         <div className="App">
             <h1>Newton School</h1>
@@ -15,7 +20,7 @@ export default function App() {
                 </form>
             </div>
             <label> {theme === "light" ? "Light Mode" : "Dark Mode"}</label>
-            <input type="checkbox" id="switch" />
+            <input type="checkbox" id="switch" onClick={toggleTheme}/>
             <label for="switch" className="label">
                 Toggle
             </label>
